b13b9ad1d24581d9ae3543330929aab43d1314f6
Terrence Tang <thtang28@gmail.com>
2018-04-11 20:18:20 -0700

    add skills

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,17 +1,11 @@
-const features = document.querySelector("#three");
-const ruby = features.querySelector(".ruby");
-const rails = features.querySelector(".rails");
+const features = document.querySelectorAll(".skills");
 
-function toggleRuby(e) {
+function toggleColor(e) {
   let tag = e.target;
   tag.classList.toggle("colored");
 }
 
-function toggleRails(e) {
-  let tag = e.target;
-  tag.classList.toggle("colored");
-}
-ruby.addEventListener('mouseover',toggleRuby);
-ruby.addEventListener('mouseout',toggleRuby);
-rails.addEventListener('mouseover',toggleRails);
-rails.addEventListener('mouseout',toggleRails);
+features.forEach(skill => {
+  skill.addEventListener('mouseover', toggleColor);
+  skill.addEventListener('mouseout', toggleColor);
+});
